26c93687270eb7975df18da7eceebf306cc2a71c
vadimlasevich <lasevichv@mail.ru>
2023-05-12 21:20:30 +0300

    Update 2023-05-12T18:20:15.827Z

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import * as flsFunctions from './modules/functions.js';
-import { burger } from './modules/burger.js';
-import { form } from './modules/form.js';
-
-flsFunctions.isWebp();
-burger();
-form();
